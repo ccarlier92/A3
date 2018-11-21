c4e16a9d000cf64408526cfaf99c1555fea67ae4
Samson971 <43796893+Samson971@users.noreply.github.com>
2018-11-22 00:37:59 +0100

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -10,6 +10,156 @@
 #include <boost/tokenizer.hpp>
 #include <string>
 
+Base * Parse_It(boost::tokenizer<boost::char_separator<char> > tokenizer commands)
+{
+	std::vector<Command*> vect_commands;
+	std::vector<Connector*> vect_connectors;
+	
+	tokenizer::iterator it = commands.begin();
+	
+	bool valid_connector = true;		//Check if there are 2 characters for && and ||
+	
+	while( it != commands.end())				//Start parsing
+	{
+		std::string actual_token = *it;	
+		std::vector<std::string> args;
+		bool is_exit = false;			//Check if the command is exit or not
+		
+		//This loop gets a command, the executable and its arguments
+		while((it != commands.end()) && (( actual_token != "&") && (actual_token != "|") && (actual_token != ";") && (actual_token != "#")))
+		{
+			args.push_back(actual_token);
+			if(actual_token == "exit" )	//if it is an exit command
+			{
+				is_exit = true;
+			}
+			it++;
+			if(it != commands.end())
+			{
+				actual_token = *it;
+			}
+		}
+		if(args.size() != 0)			//Check if it was not a single command or not
+		{
+			Command * command;
+			if(is_exit == true)
+			{
+				command = new Exit(args);	
+			}
+			else
+			{
+				command = new Command(args);
+			}
+			vect_commands.push_back(command);	//add the command to the vector
+		}
+
+
+		if(it != commands.end())
+		{
+			//When a connector or comment token is reached
+			if(actual_token == "#")
+			{
+				//Don't need what's next so break
+				break;
+			}
+			
+			//if it is a connector ==> create it
+			//add it to the vector
+			//if & or | conectors, go to next iterator which will end up being skipped
+			if(actual_token == "&")
+			{	
+				it++;
+				if (it == commands.end())
+				{
+					valid_connector = false;
+					break;
+				}
+				else if (actual_token != *it)
+				{
+					valid_connector = false;
+					break;
+				}
+				else
+				{
+					Connector * connector = new And();
+					vect_connectors.push_back(connector);
+				}
+			}
+			else if(actual_token == "|")
+			{
+				it++;
+				if (it == commands.end())
+				{
+					valid_connector = false;
+					break;
+				}
+				else if (actual_token != *it)
+				{
+					valid_connector = false;
+					break;
+				}
+				else
+				{	
+					Connector * connector = new Or();
+					vect_connectors.push_back(connector);
+				}
+			}
+			else if(actual_token == ";")
+			{
+				Connector * connector = new SemiColon();
+				vect_connectors.push_back(connector);
+			}			
+		}	
+			
+		if(it != commands.end())
+		{
+			it++;
+		}
+	}
+	
+	//set the composite tree
+	if ((vect_connectors.size()== (vect_commands.size()-1)) && (vect_commands.size()!=0) && (valid_connector == true))	//Check if the command line is a good input
+	{ 
+		if(vect_connectors.size()!=0) 	//means several commands
+		{	
+			for(int i = 0; i< vect_connectors.size(); i++)
+			{
+				//append the connectors to the right children (commands or connectors)
+				if(i==0)
+				{
+					vect_connectors[i]->append(vect_commands[i],vect_commands[i+1]);
+				}
+				else
+				{
+					vect_connectors[i]->append(vect_connectors[i-1],vect_commands[i+1]);
+				}
+			}
+
+			//return the connector on top of the tree, the last one
+			res = vect_connectors[vect_connectors.size()-1];
+		}
+		else	//means one command
+		{
+			//return the only command
+			res = vect_commands[0];
+		}
+	}
+	else	//means the command line was inedaquate 
+	{
+		//return null
+		res = NULL;
+		if (vect_connectors.size()!= (vect_commands.size()-1))
+		{
+			std::cout<<"Invalid Input"<<std::endl;
+		}
+		else if(valid_connector == false)
+		{
+			std::cout<<"Invalid Connector"<<std::endl;
+		}
+	}
+	return res;
+}
+
 
 Base * Parse(std::string command_line)
 {
