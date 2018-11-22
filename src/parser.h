41bde5f2a133abe69bb0d20bab18844b4fdf2b9d
Samson971 <43796893+Samson971@users.noreply.github.com>
2018-11-22 03:32:48 +0100

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -21,7 +21,7 @@ Base * Parse_It(boost::tokenizer<boost::char_separator<char> > commands , boost:
 	
 	bool valid_connector = true;		//Check if there are 2 characters for && and ||
 	
-	while( it != commands.end() || (in_parenthesis && *it == ")"))				//Start parsing
+	while( it != commands.end() || (in_parenthesis && *it != ")"))				//Start parsing
 	{
 		if(in_parenthesis)
 		{
@@ -40,7 +40,7 @@ Base * Parse_It(boost::tokenizer<boost::char_separator<char> > commands , boost:
 				is_exit = true;
 			}
 			it++;
-			if(it != commands.end() || (in_parenthesis && *it == ")"))
+			if(it != commands.end() || (in_parenthesis && *it != ")"))
 			{
 				actual_token = *it;
 			}
@@ -60,7 +60,7 @@ Base * Parse_It(boost::tokenizer<boost::char_separator<char> > commands , boost:
 		}
 
 
-		if(it != commands.end() || (in_parenthesis && *it == ")"))
+		if(it != commands.end() || (in_parenthesis && *it != ")"))
 		{
 			//When a connector or comment token is reached
 			if(actual_token == "#")
@@ -75,7 +75,7 @@ Base * Parse_It(boost::tokenizer<boost::char_separator<char> > commands , boost:
 			if(actual_token == "&")
 			{	
 				it++;
-				if (it == commands.end() || (in_parenthesis && *it == ")"))
+				if (it == commands.end() || (in_parenthesis && *it != ")"))
 				{
 					valid_connector = false;
 					break;
@@ -94,7 +94,7 @@ Base * Parse_It(boost::tokenizer<boost::char_separator<char> > commands , boost:
 			else if(actual_token == "|")
 			{
 				it++;
-				if (it == commands.end() || (in_parenthesis && *it == ")"))
+				if (it == commands.end() || (in_parenthesis && *it != ")"))
 				{
 					valid_connector = false;
 					break;
