fb0281351ff1aad71b3d05379a78cbfc638d6cf9
Samson971 <43796893+Samson971@users.noreply.github.com>
2018-11-27 00:23:21 +0100

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -165,7 +165,7 @@ Base * Parse_It(boost::tokenizer<boost::char_separator<char> > commands , boost:
 		res = NULL;
 		if (vect_connectors.size()!= (vect_commands.size()-1))
 		{
-			std::cout<<"Invalid Input"<<std::endl;
+			std::cout<<"Invalid Input"<<std::endl<<"connectors "<<vect_connectors.size() <<std::endl "commands "<<vect_commands.size()<<std::endl;
 		}
 		else if(valid_connector == false)
 		{
