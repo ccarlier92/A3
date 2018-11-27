1dab9d5d982c69fb0e23bedce4684473bcc1e43d
Samson971 <43796893+Samson971@users.noreply.github.com>
2018-11-27 04:23:21 +0100

    Update testCommand.h

--- src/testCommand.h
+++ src/testCommand.h
@@ -7,7 +7,7 @@ class Test : public Command{
 	public :
 		Test(std::vector<std::string> args):Command(args){};
 		bool execute();
-		bool is_flag(std::string arg);
+		bool is_flag(char arg);
 };
 
 #endif
