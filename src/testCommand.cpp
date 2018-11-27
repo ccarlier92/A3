c2471836b52b211f144ccd8facd1360e66b9d4e0
Samson971 <43796893+Samson971@users.noreply.github.com>
2018-11-27 07:37:36 +0100

    Update testCommand.cpp

--- src/testCommand.cpp
+++ src/testCommand.cpp
@@ -8,37 +8,43 @@ bool Test::execute()
 {
   bool res = false;
   struct stat buffer;   
-  
-  if(is_flag(args[1]) == false || args[2] == "-e")
-  {
-	if(stat(args[1], &buffer) == 0)
+	if(args[2] == NULL || (is_flag(args[1]) && args[1] == "-e"))
 	{
-	       res = true;
+		if(stat(args[1], &buffer) == 0)
+		{
+		       res = true;
+		}  
 	}
 	else
 	{
-	       res = false;
+		if(is_flag(args[1])
+		{
+			if(args[1] == "-d")
+			{
+			res = S_ISDIR(buffer.st_mode);
+			}
+
+			else if(args[1] == "-f")
+			{
+			res = S_ISREG(buffer.st_mode);
+			}
+
+			else
+			{
+			std::cout<<"Invalid input"<<std::endl;
+			}  
+		}
 	}
-  }
-  
-  else if(args[1] == "-d")
-  {
-    res = S_ISDIR(buffer.st_mode);
-  }
-  
-  else if(args[1] == "-f")
-  {
-    res = S_ISREG(buffer.st_mode);
-  }
-  
-  else
-  {
-     std::cout<<"Invalid input"<<std::endl;
-     return(false);
-  }
-  
-std::cout<<"( "<< res <<" )"<<std::endl;
-return(res);
+	if(res == true)
+	{
+		std::cout<<"( "<< TRUE <<" )"<<std::endl;
+	}
+	else
+	{
+		std::cout<<"( "<< FALSE <<" )"<<std::endl;
+	}
+
+	return(res);
 }
 
 bool Test::is_flag(char* value)
